0f62bb2cbb0697271ec82886061282387c1979ea
Alexandre <alexandre.angleraud@gmail.com>
2020-02-19 16:29:58 +0200

    Improved controls

--- cobot_controllers/scripts/test_bench.py
+++ cobot_controllers/scripts/test_bench.py
@@ -17,6 +17,7 @@ from moveit_commander.conversions import pose_to_list
 from control_msgs.msg import GripperCommandActionGoal, GripperCommandGoal, GripperCommand
 from franka_gripper.msg import HomingAction, HomingActionGoal, GraspAction, GraspActionGoal, GraspGoal, MoveActionGoal, MoveGoal
 
+from franka_control.msg import ErrorRecoveryActionGoal
 
 class TestBench(object):
     def __init__(self):
@@ -27,18 +28,23 @@ class TestBench(object):
       self.scene = moveit_commander.PlanningSceneInterface()
       self.group = moveit_commander.MoveGroupCommander(group_name)
 
+      self.reset_sub = rospy.Subscriber("/reset_cmd", Empty, self.reset)
       self.x_test_sub = rospy.Subscriber("/x_test", Float32, self.test_x)
       self.start_sub = rospy.Subscriber("/test_bench", Test, self.routine)
       self.homing_sub = rospy.Subscriber("/homing_cmd", Empty, self.homing)
+      self.stop_sub = rospy.Subscriber("/stop_cmd", Empty, self.stop_routine)
       self.approach_sub = rospy.Subscriber("/approach_cmd", Empty, self.approach)
       self.height_test_sub = rospy.Subscriber("/height_test", Float32, self.test_height)
 
       self.open_gripper_pub = rospy.Publisher("/franka_gripper/move/goal", MoveActionGoal, queue_size=10)
       self.gripper_grasp_client = rospy.Publisher('/franka_gripper/grasp/goal', GraspActionGoal, queue_size=10)
+      self.recovery_pub = rospy.Publisher("/franka_control/error_recovery/goal", ErrorRecoveryActionGoal, queue_size=10)
       self.generic_grasp_client =  rospy.Publisher('/franka_gripper/gripper_action/goal', GripperCommandActionGoal, queue_size=10)
 
       self.gripper_homing_client = actionlib.SimpleActionClient('/franka_gripper/homing', HomingAction)
       self.gripper_homing_client.wait_for_server()
+
+      self.carry_on_routine = True
 
     def homing(self, msg):
         goal = HomingActionGoal(goal={})
@@ -51,17 +57,27 @@ class TestBench(object):
         gripper_goal.speed = 20.0
         gripper_goal.width = 0.08
         self.open_gripper_pub.publish(MoveActionGoal(goal=gripper_goal))
+        if not self.carry_on_routine:
+            raise ValueError
+
+    def reset(self, msg):
+        self.carry_on_routine = True
+        recovery_goal = ErrorRecoveryActionGoal()
+        self.recovery_pub.publish(recovery_goal)
 
     def grasp(self, width, force):
         ''' width, epsilon_inner, epsilon_outer, speed, force '''
-        grasp_goal = GraspGoal()
-        grasp_goal.width = float(width)
-        grasp_goal.epsilon.inner = 0.005
-        grasp_goal.epsilon.outer = 0.005
-        grasp_goal.speed = 20.0
-        grasp_goal.force = float(force)
-        grasp_msg = GraspActionGoal(goal=grasp_goal)
-        self.gripper_grasp_client.publish(grasp_msg)
+        if self.carry_on_routine:
+            grasp_goal = GraspGoal()
+            grasp_goal.width = float(width)
+            grasp_goal.epsilon.inner = 0.005
+            grasp_goal.epsilon.outer = 0.005
+            grasp_goal.speed = 20.0
+            grasp_goal.force = float(force)
+            grasp_msg = GraspActionGoal(goal=grasp_goal)
+            self.gripper_grasp_client.publish(grasp_msg)
+        else:
+            raise ValueError
 
     def grasp2(self, width, force):
         ''' width, epsilon_inner, epsilon_outer, speed, force '''
@@ -86,10 +102,21 @@ class TestBench(object):
         self.clear_group()
 
     def vertical_move(self, value):
-        next_point = self.group.get_current_pose().pose
-        next_point.position.z += value/100
-        self.move_group(next_point)
-        self.clear_group()
+        if self.carry_on_routine:
+            next_point = self.group.get_current_pose().pose
+            next_point.position.z += value/100
+            self.move_group(next_point)
+            self.clear_group()
+        else:
+            raise ValueError
+            
+    def place(self):
+        if self.carry_on_routine:
+            self.group.set_named_target("low_pose")
+            self.group.go()
+            self.group.stop()
+        else:
+            raise ValueError
 
     def approach(self, msg):
         self.group.set_max_velocity_scaling_factor(0.75)
@@ -112,6 +139,9 @@ class TestBench(object):
         self.group.stop()
         self.group.clear_pose_targets()
 
+    def stop_routine(self, msg):
+        self.carry_on_routine = False
+
     def routine(self, msg):
         '''
         '''
@@ -133,32 +163,32 @@ class TestBench(object):
         print("Desired v_interval: {}m".format(v_interval))
         print()
         print("=============================================")
-        for set in range(0, sets):
-            print("Horizontal_move")
-            for rep in range(0, reps):
-                self.group.remember_joint_values("low_pose")
-                self.grasp2(width, force)
-                time.sleep(1)
-                self.approach({})
-                time.sleep(1)
-                #self.vertical_move(-0.13)
-                self.group.set_named_target("low_pose")
-                self.group.go()
-                self.group.stop()
-                self.release()
-                time.sleep(1)
-                self.vertical_move(-10)
-                #self.homing({})
-                self.group.set_named_target("low_pose")
-                self.group.go()
-                self.group.stop()
-                if (rep<reps-1):
-                    self.horizontal_move(h_interval)
-            if (set<sets-1):
-                self.horizontal_move(-(reps-1)*h_interval)
-                self.vertical_move(v_interval)
-            else:
-                self.approach({})
+        try:
+            for set in range(0, sets):
+                print("Horizontal_move")
+                for rep in range(0, reps):
+                    self.group.remember_joint_values("low_pose")
+                    self.grasp2(width, force)
+                    time.sleep(1)
+                    self.approach({})
+                    time.sleep(1)
+                    #self.vertical_move(-0.13)
+                    self.place()
+                    self.release()
+                    time.sleep(1)
+                    self.vertical_move(-10)
+                    #self.homing({})
+                    self.place()
+                    if (rep<reps-1):
+                        self.horizontal_move(h_interval)
+                if (set<sets-1):
+                    self.horizontal_move(-(reps-1)*h_interval)
+                    self.vertical_move(v_interval)
+                else:
+                    self.approach({})
+        except:
+            print("Stopped")
+
 
 if __name__ == '__main__':
     rospy.init_node('cobot_control')
