0574ea87351152532b22997ff7ac7cde3930c886
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-04-07 18:24:48 +0300

    switch to HTN planning

--- cobot_control/scripts/handover_demo.py
+++ cobot_control/scripts/handover_demo.py
@@ -2,26 +2,25 @@
 import time
 import rospy
 from cobot_control.planner import RosPlanner
-from cobot_control.dispatcher import Dispatcher
 from cobot_tuni_msgs.msg import Command, Object, Collection
 
 class Tutorial(object):
 
     def __init__(self):
         rospy.init_node('handover_demo', anonymous=True)
-        self.planner = RosPlanner('localhost:3030', 'Panda')
         self.obj_detector_publisher = rospy.Publisher('/object', Object, queue_size=10)
         self.cmd_publisher = rospy.Publisher('/nlp_command', Command, queue_size=10)
         self.action_publisher = rospy.Publisher('/semantic_action', Command, queue_size=10)
         self.plan_subscriber = rospy.Subscriber('/semantic_command', Command, self.create_plan)
+        pose_estimator = rospy.ServiceProxy('calculate_tag_pose', EstimatePose)
+        sem_controller = SemanticController(host, dataset, pose_estimator)
+        self.planner = RosPlanner('localhost:3030', 'Panda', self.action_publisher, sem_controller)
 
     def create_plan(self, cmd):
         print("create_plan {}".format(cmd.action))
         self.planner.create_plan(cmd.action)
+        self.planner.run(plan)
 
-    def apply_timestamp(self, cmd, dispatcher):
-        print("done {}".format(cmd.action))
-        dispatcher.apply_timestamp(cmd.action)
 
 def main():
     try:
@@ -40,20 +39,8 @@ def main():
 
         msg = Command("Give", "Cup")
         tuto.cmd_publisher.publish(msg)
-        print "============ Press `Enter` to visualize resulting changes in the KB ..."
 
-        print "============ Press `Enter` to execute the command ..."
-        raw_input()
-        dispatcher = Dispatcher(tuto.planner.retrieve_plan(), 'localhost:3030', 'Panda')
-        action_subscriber = rospy.Subscriber('/action_command', Command, tuto.apply_timestamp, (dispatcher))
-        for task, object in dispatcher.dispatch():
-            print("dispatching {} -{}".format(task,object))
-            tuto.action_publisher.publish(Command(task, object))
-        time.sleep(2)
         print "============ Press `Enter` to release the object ..."
-        raw_input()
-
-        print "============ Press `Enter` to clean the knowledge base ..."
         raw_input()
 
         print "============ Demo complete!"
--- cobot_control/src/cobot_control/planner.py
+++ cobot_control/src/cobot_control/planner.py
@@ -1,36 +1,89 @@
 from simple_net.planner import Planner
 from sem_server_ros.server_com import FusekiEndpoint
 from sem_server_ros.queries import QueryTemplateEngine
+from cobot_controllers.semantic_controller import SemanticController
+from cobot_tuni_msgs.msg import Command
+
+class DispatchingError(Exception):
+   def __init__(self, primitive):
+      self.primitive = primitive
 
 class RosPlanner:
 
-    def __init__(self, host, dataset):
-        self.planner = Planner()
+    def __init__(self, host, dataset, sem_controller):
+        self.final_plan = []
+        self.decomp_history = []
+        self.working_world_state = []
+        self.sem_controller = sem_controller
         self.sem_server = FusekiEndpoint(host, dataset)
         self.query_engine = QueryTemplateEngine(__file__)
+        self.action_publisher = action_publisher
 
-    def retrieve_plan(self):
-        try:
-            return self.planner.base_solution
-        except Exception as e:
-            print(e)
+    def find_type(self, task):
+        pass
 
-    def find_available_steps(self, time):
-        try:
-            return self.planner.find_available_steps(time)
-        except Exception as e:
-            print(e)
+    def find_preconditions(self, task):
+        pass
+
+    def find_effects(self, task):
+        pass
+
+    def are_preconditions_met(self, primitive):
+        pass
+
+    def find_satisfied_method(self):
+        template = self.query_engine.load_template('select_semantic_plan.rq')
+        query = self.query_engine.generate(template, sem_command)
+        result = self.sem_server.construct_data(query)
+        return result
+
+    def find_subtasks(self, method):
+        pass
+
+    def record_decomposition_of_task(pass, current_task, final_plan):
+        pass
+
+    def restore_to_last_decomposed_task(self):
+        pass
+
+    def apply_effects(self, primitive):
+        pass
 
     def create_plan(self, sem_command):
         try:
-            template = self.query_engine.load_template('select_semantic_plan.rq')
-            query = self.query_engine.generate(template, sem_command)
-            result = self.sem_server.construct_data(query)
-            print("R E S U L T  P L A N:{}".format(result))
-            return self.planner.create_plan(result)
-        except:
-            print(self.planner.base_solution)
-            print("Error during planning")
+            self.final_plan = []
+            tasks_to_process = list(sem_command)
+            while tasks_to_process:
+                current_task = tasks_to_process.pop(0)
+                if self.find_type(current_task) == "CompoundTask":
+                    method = self.find_satisfied_method()
+                    if method:
+                        self.record_decomposition_of_task(current_task, self.final_plan)
+                        subtasks = self.find_subtasks(method)
+                        tasks_to_process.insert(subtasks)
+                    else:
+                        self.restore_to_last_decomposed_task()
+                else:  # Primitive task
+                    if self.are_preconditions_met(current_task):
+                        self.apply_effects(current_task)
+                        self.final_plan.append(current_task)
+                    else:
+                        self.restore_to_last_decomposed_task()
+        except Exception as e:
+            print(e)
 
-    def print_plan(self):
-        self.planner.print_plan()
+    def execute(self, primitive):
+        sem_controller.interpret(primitive)
+
+    def run(self, plan):
+        try:
+            while self.final_plan:
+                primitive = self.final_plan.pop(0)
+                if self.are_preconditions_met(primitive):
+                    self.execute(primitive)
+                else:
+                    raise DispatchingError(primitive)
+        except DispatchingError as e:
+            self.final_plan.insert(0, e.primitive)
+            self.create_plan(self.final_plan)
+            self.run()
