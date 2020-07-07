ec92d649e7325ef0c44608d2082379bd90515387
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-07-07 11:55:45 +0300

    Allows voice input

--- cobot_nlp/scripts/speech_to_text_node.py
+++ cobot_nlp/scripts/speech_to_text_node.py
@@ -9,9 +9,9 @@ def recordAudio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
-        print("Say something!")
-        audio = r.listen(source, phrase_time_limit=10)
-
+        #audio = r.listen(source, phrase_time_limit=10)
+        audio = r.listen(source)
+    print("Listening")
     data = ""
     try:
         # Uses the default API key
@@ -21,7 +21,6 @@ def recordAudio():
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
     return data
 
 if __name__ == '__main__':
@@ -29,7 +28,10 @@ if __name__ == '__main__':
         rospy.init_node('speech_to_text_input')
         publisher = rospy.Publisher('/command', String, queue_size=10)
         while not rospy.is_shutdown():
+
             data = recordAudio()
-            publisher.publish(data)
+            if data:
+                print("I heard: {}".format(data))
+                publisher.publish(data)
     except rospy.ROSInterruptException:
         pass
