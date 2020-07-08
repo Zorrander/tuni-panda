12a838f5919e92368c965ed4653cbbc0fa0f7964
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-07-08 16:15:44 +0300

    reach and close on demand

--- cobot_nlp/scripts/speech_to_text_node.py
+++ cobot_nlp/scripts/speech_to_text_node.py
@@ -1,37 +1,50 @@
 #!/usr/bin/env python
-
+import time
 import rospy
 import speech_recognition as sr
 from std_msgs.msg import String
+from os import path
 
+publisher = rospy.Publisher('/command', String, queue_size=10)
 
-def recordAudio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        #audio = r.listen(source, phrase_time_limit=10)
-        audio = r.listen(source)
-    print("Listening")
-    data = ""
+def callbackAudio(recognizer, audio):
+    # received audio data, now we'll recognize it using Google Speech Recognition
     try:
-        # Uses the default API key
-        # To use another API key: `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        data = r.recognize_google(audio)
+        print("Google Speech Recognition thinks you said ")
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        cmd = recognizer.recognize_google(audio)
+        print( cmd)
+        #publisher.publish(cmd)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    return data
+
+def sphinx_callback(recognizer, audio):
+    # recognize speech using Sphinx
+    try:
+        # print("Sphinx thinks you said " + recognizer.recognize_sphinx(audio, grammar=path.join(path.dirname(path.realpath(__file__)), 'counting.gram')))
+        cmd = recognizer.recognize_sphinx(audio, keyword_entries=[("pass", 1.0), ("reach", 1.0), ("grasp", 1.0), ("open", 1.0)])
+        print(cmd.strip())
+        publisher.publish(cmd.strip())
+        # print("Sphinx thinks you said " + recognizer.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+    print("Say something")
+
 
 if __name__ == '__main__':
+    rospy.init_node('speech_to_text_input')
     try:
-        rospy.init_node('speech_to_text_input')
-        publisher = rospy.Publisher('/command', String, queue_size=10)
-        while not rospy.is_shutdown():
-
-            data = recordAudio()
-            if data:
-                print("I heard: {}".format(data))
-                publisher.publish(data)
+        r = sr.Recognizer()
+        m = sr.Microphone()
+        with m as source:
+            r.adjust_for_ambient_noise(source, duration=10)
+        stop_listening = r.listen_in_background(m, sphinx_callback)
+        rospy.spin()
     except rospy.ROSInterruptException:
-        pass
+        stop_listening(wait_for_stop=False)
