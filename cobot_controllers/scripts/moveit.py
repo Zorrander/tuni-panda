8e8a2c3a97bcc607b068391ddff48b26c4a57591
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-08-03 17:48:54 +0300

    reset mechanism

--- cobot_controllers/scripts/moveit.py
+++ cobot_controllers/scripts/moveit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
-from std_srvs.srv import Trigger
+from std_srvs.srv import Trigger, TriggerResponse
 from cobot_controllers.arm import Arm
 from cobot_msgs.srv import *
+from franka_control.msg import ErrorRecoveryActionGoal
 
 test_joint_positions_array = [-0.000190902556141577, -0.6355551061881216, 0.0005800443025828713,
                     -2.6969856774848804, 7.176671650806756e-05, 2.0608427823384603, 0.7847883398630692]
@@ -40,16 +41,24 @@ def list_targets(request, robot):
     print("Received request")
     return ListTargetsResponse(robot.list_targets())
 
+def reset(request, pub):
+    print("Received reset request")
+    pub.publish(ErrorRecoveryActionGoal())
+    return TriggerResponse(True, "Robot recovery")
+
 if __name__ == '__main__':
     rospy.init_node('panda_arm_control')
     panda_arm = Arm()
 
-    s2 = rospy.Service("go_to_cartesian_goal", ReachCartesianPose, lambda msg: go_to_cartesian_goal(msg,panda_arm))
+    pub = rospy.Publisher('/franka_control/error_recovery/goal', ErrorRecoveryActionGoal, queue_size=10)
+
     s1 = rospy.Service('go_to_joint_space_goal', ReachJointPose, lambda msg: go_to_joint_space_goal(msg,panda_arm))
+    s2 = rospy.Service("go_to_cartesian_goal", ReachCartesianPose, lambda msg: go_to_cartesian_goal(msg,panda_arm))
     s3 = rospy.Service('move_to', NamedTarget, lambda msg: move_to(msg,panda_arm) )
     s4 = rospy.Service("store_position", NamedTarget, lambda msg: store_position(msg,panda_arm) )
     s5 = rospy.Service('set_speed', RobotSpeed, lambda msg: set_speed(msg,panda_arm) )
     s6 = rospy.Service('get_targets', ListTargets, lambda msg: list_targets(msg,panda_arm) )
+    s7 = rospy.Service('reset', Trigger, lambda msg: reset(msg, pub) )
 
     print("Arm ready")
     rospy.spin()
