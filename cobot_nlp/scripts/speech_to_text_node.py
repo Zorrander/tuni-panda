0f6a69fd933b30830f7affc669f34852f77fb17a
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-08-18 17:31:27 +0300

    recognize keywords

--- cobot_nlp/scripts/speech_to_text_node.py
+++ cobot_nlp/scripts/speech_to_text_node.py
@@ -15,8 +15,8 @@ def callbackAudio(recognizer, audio):
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         cmd = recognizer.recognize_google(audio)
-        print( cmd)
-        #publisher.publish(cmd)
+        print(cmd)
+        publisher.publish(cmd)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
@@ -25,10 +25,15 @@ def callbackAudio(recognizer, audio):
 def sphinx_callback(recognizer, audio):
     # recognize speech using Sphinx
     try:
+        print("Think")
         # print("Sphinx thinks you said " + recognizer.recognize_sphinx(audio, grammar=path.join(path.dirname(path.realpath(__file__)), 'counting.gram')))
-        cmd = recognizer.recognize_sphinx(audio, keyword_entries=[("pass", 1.0), ("reach", 1.0), ("grasp", 1.0), ("open", 1.0)])
-        print(cmd.strip())
-        publisher.publish(cmd.strip())
+        cmd = recognizer.recognize_sphinx(audio, keyword_entries=[("pass", 1.0), ("reach", 1.0), ("grasp", 1.0), ("open", 1.0), ("peg", 1.0)])
+        bow = cmd.strip().split(' ')
+        print(bow)
+        print(type(bow))
+        if type(bow) == list:
+            print(bow[2], bow[0])
+        publisher.publish(bow[2] + ' ' + bow[0])
         # print("Sphinx thinks you said " + recognizer.recognize_sphinx(audio))
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")
@@ -41,10 +46,12 @@ if __name__ == '__main__':
     rospy.init_node('speech_to_text_input')
     try:
         r = sr.Recognizer()
-        m = sr.Microphone()
+        m = sr.Microphone(device_index=11)
         with m as source:
-            r.adjust_for_ambient_noise(source, duration=10)
-        stop_listening = r.listen_in_background(m, sphinx_callback)
-        rospy.spin()
+            r.adjust_for_ambient_noise(source, duration=2)
+            while not rospy.is_shutdown():
+                print("say something!")
+                audio = r.listen(m)
+                sphinx_callback(r, audio)
     except rospy.ROSInterruptException:
         stop_listening(wait_for_stop=False)
