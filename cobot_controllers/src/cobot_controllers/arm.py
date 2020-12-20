1b213f209ee72218620dbd5ded496bb7071b6da7
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-12-20 21:10:17 +0200

    Kitting2

--- cobot_controllers/src/cobot_controllers/arm.py
+++ cobot_controllers/src/cobot_controllers/arm.py
@@ -71,7 +71,13 @@ class Arm(object):
 
     def move_to(self, target_name):
         ''' Send robot to a predefined position'''
+        self.set_speed(0.75)
         try:
+            self.group.set_named_target("part_approach")
+            self.group.go()
+            self.group.stop()
+            self.group.clear_pose_targets()
+
             self.group.set_named_target(target_name)
             self.group.go()
             self.group.stop()
